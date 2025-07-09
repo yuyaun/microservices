7ed9255f975b5218cc40c1c2a5a0c338d4310131
Ian <ian@brobridge.com>
2025-07-09 14:44:11 +0800

    Fix formatting in router.py and update import in database.py; add pytest to requirements

--- app/api/internal/v1/router.py
+++ app/api/internal/v1/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
 from app.api.internal.v1.endpoints import healthcheck
 
-router = APIRouter()router.include_router(healthcheck.router, tags=["internal:health"])
+router = APIRouter()
+router.include_router(healthcheck.router, tags=["internal:health"])
